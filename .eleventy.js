27e5fe3626ae8783b26611e219f461f4da739090
Ensle Rene <ensle.rene@gmail.com>
2025-10-13 17:37:36 +0200

    continue implementing

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/output.css");
     eleventyConfig.addPassthroughCopy({ "./node_modules/preline/dist": "assets/js/preline" });
+    eleventyConfig.addPassthroughCopy({
+  "./node_modules/@fontsource-variable/inter/files": "files"
+});
     eleventyConfig.addPassthroughCopy({ "./src/assets": "assets/" });
     eleventyConfig.addFilter("breadcrumbs", function (url) {
         if (!url) return [];
@@ -23,8 +26,16 @@ module.exports = function (eleventyConfig) {
         return breadcrumbs;
     });
 
+    eleventyConfig.addFilter('replaceAll', function(str, find, replace) {
+  if (!str) return '';
+  return str.split(find).join(replace);
+});
+
     return {
         pathPrefix: "/eleventy-ev/",
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
         dir: {
             input: "src",
             output: "docs"
---
+++ src/sites/Folienverpackung/Folienverpackung-für-Sonstige.md
@@ -0,0 +1,11 @@
+---
+tags: folienverpackung
+name: Folienverpackung für Sonstige
+subfolder: Folienverpackung-für-Sonstige
+image: /assets/img/machines/kartonsammelpacker.jpg
+description: |
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+permalink: false
+---
